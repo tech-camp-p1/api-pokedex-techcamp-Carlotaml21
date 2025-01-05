6e98b2ce3d458214344b2185c7153374faf34bc1
Carlotaml21 <carlotaml21@gmail.com>
2025-01-05 15:31:28 +0100

    add javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const pokemon_count = 151;
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -16,4 +16,47 @@ const colors = {
 	fighting: '#E6E0D4',
 	normal: '#F5F5F5'
 }
+const defaultColor = '#f0f0f0';
+
+async function fetchPokemons() {
+	for (let i = 1; i <= pokemon_count; i++) {
+	  await fetchPokemon(i);
+	}
+  }
+  async function fetchPokemon(id) {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`; 
+	const response = await fetch(url); 
+	const data = await response.json(); 
+	createPokemonCard(data);
+  }
+ 
+function createPokemonCard(pokemon) {
+	const pokemonEl = document.createElement("div"); 
+	pokemonEl.classList.add("pokemon"); 
+  
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+	const id = pokemon.id.toString().padStart(3, "0"); 
+	const pokeType = pokemon.types[0].type.name; 
+	const color = colors[pokeType] || defaultColor; 
+  
+	pokemonEl.style.backgroundColor = color; 
+  
+	
+	const pokemonInnerHTML = `
+	  <div class="img-container">
+		<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="${name}">
+	  </div>
+	  <div class="info">
+		<span class="number">#${id}</span>
+		<h3 class="name">${name}</h3>
+		<small class="type">Type: <span>${pokeType}</span></small>
+	  </div>
+	`;
+  
+	pokemonEl.innerHTML = pokemonInnerHTML; 
+	poke_container.appendChild(pokemonEl);
+  }
+
+  fetchPokemons();
+  
 
